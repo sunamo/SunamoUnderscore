719d3ffab40a38cc15f56f1da716811cf93bada7
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-05-11 11:16:13 +0200

    Some projects are flat and buildable

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -2,3 +2,9 @@ global using SunamoEnums.Enums;
 global using System.Collections.Generic;
 global using System;
 global using System.Threading.Tasks;
+global using System.Text;
+global using System.Collections.Generic;
+global using System.Collections;
+global using System.IO;
+global using System.Linq;
+
