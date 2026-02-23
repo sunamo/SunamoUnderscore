3a2718e23ebe19ff08e7561c0a966b6e136ba605
Radek Jancik <radek.jancik@usys.cz>
2026-02-23 13:31:49 +0100

    26.2.23.3 feat: fixing variables names

--- RunnerUnderscore/Program.cs
+++ RunnerUnderscore/Program.cs
@@ -1,3 +1,4 @@
+// variables names: ok
 namespace RunnerUnderscore;
 
 internal class Program
--- SunamoUnderscore/IGoPayOAuth.cs
+++ SunamoUnderscore/IGoPayOAuth.cs
@@ -1,3 +1,4 @@
+// variables names: ok
 namespace SunamoUnderscore;
 
 public interface IGoPayOAuth : IOAuth
